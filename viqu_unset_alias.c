755acdd7b627d0c9f03b495fba9b9eeec6fe1045
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-12 07:05:12 -0800

    Added Function for the removal of an alias specified by a string

---
+++ viqu_unset_alias.c
@@ -0,0 +1,27 @@
+/**
+ * viqu_unset_alias - Function to remove an alias specified by a string.
+ * @viqu_info: Structure containing parameters.
+ * @viqu_str: The string representing the alias.
+ *
+ * Return: 0 when successful or (1) on error.
+ */
+int viqu_unset_alias(viqu_info_t *viqu_info, char *viqu_str)
+{
+	char *viqu_p, viqu_c;
+	int viqu_ret;
+
+	viqu_p = viqu_get_str_char(viqu_str, '=');
+	if (!viqu_p)
+		return (1);
+
+	viqu_c = *viqu_p;
+	*viqu_p = 0;
+
+	viqu_ret = viqu_del_node_idx(&(viqu_info->viqu_alias),
+			gets_index_node(viqu_info->viqu_alias,
+				viqu_prefix_node(viqu_info->viqu_alias, viqu_str, -1)));
+
+	*viqu_p = viqu_c;
+
+	return (viqu_ret);
+}
