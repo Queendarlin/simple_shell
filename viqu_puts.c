023eb0569203f2dedb00e0941bb658a2428c0d6b
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-12 06:53:24 -0800

    Added Function to print a string to the standard output

---
+++ viqu_puts.c
@@ -0,0 +1,20 @@
+#include “shell.h”
+
+/**
+ * viqu_puts - Function to print a string to the standard output.
+ * @viqu_str: Pointer to the string to be printed.
+ *
+ * Return: void
+ */
+void viqu_puts(char *viqu_str)
+{
+	Int viqu_index;
+
+	if (!viqu_str)
+		return;
+
+	for (viqu_index = 0; viqu_str[viqu_index] != '\0'; viqu_index++)
+	{
+		viqu_putchar(viqu_str[viqu_index]);
+	}
+}
