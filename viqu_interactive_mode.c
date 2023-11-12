ebc6fa45862b7e6abcfa417b5806dec81ad213c5
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-12 08:32:11 -0800

    Added Function to show if the shell is in interactive mode

---
+++ viqu_interactive_mode.c
@@ -0,0 +1,11 @@
+#include “shell.h
+
+/**
+ * viqu_interactive_mode - Function to show if the shell is in interactive mode
+ * @viqu_info: Address of the structure containing shell information.
+ * Return: 1 if the shell is in interactive mode, else 0
+ */
+int viqu_interactive_mode(info_t *viqu_info)
+{
+	return (viqu_isatty(STDIN_FILENO) && viqu_info->viqu_read_fd <= 2);
+}
