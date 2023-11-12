5d3c4c09a86ee079dfb7b713d5e1c53863f53b52
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-12 06:19:49 -0800

    Added function to check if the substring starts with a certain string

---
+++ viqu_string_start.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * viqu_string_start - To check if the substring starts with a certain string
+ * @viqu_haystack: The string to search.
+ * @viqu_needle: The substring to find.
+ *
+ * Return: The address of the next character in viqu_haystack if viqu_needle
+ * starts with viqu_haystack, or NULL otherwise.
+ */
+char *viqu_string_start(const char *viqu_haystack, const char *viqu_needle)
+{
+	while (*viqu_needle)
+	{
+		if (*viqu_needle++ != *viqu_haystack++)
+			return (NULL);
+	}
+
+	return ((char *)viqu_haystack);
+}
