0eaa63cdcd6b21583ebe4a1b1eb0db4037945c5d
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-12 08:50:43 -0800

    Added Function to populate the environment linked list

---
+++ viqu_occupy_env.c
@@ -0,0 +1,20 @@
+#include “shell.h”
+
+/**
+ * viqu_occupy_environ_list - Function to populate the environment linked list.
+ * @viqu_info: Structure containing information related to potential arguments.
+ *
+ * Return: Always returns 0.
+ */
+int viqu_occupy_environ_list(info_t *viqu_info)
+{
+	list_t *viqu_node = NULL;
+	size_t viqu_index;
+
+	for (viqu_index = 0; viqu_environ[viqu_index]; viqu_index++)
+		viqu_suffix_node(&viqu_node, viqu_environ[viqu_index], 0);
+
+	viqu_info->viqu_env = viqu_node;
+
+	return (0);
+}
