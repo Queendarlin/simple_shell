d714eb7fc6931b65ab16fc59857f0413498ee320
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-13 08:33:59 -0800

    debugging

--- viqu_free_list_field.c
+++ viqu_free_list_field.c
@@ -27,7 +27,7 @@ void viqu_free_list_field(info_t *viqu_info, int viqu_every)
 			viqu_free_all_nodes(&(viqu_info->viqu_alias));
 
 		viqu_free_web_string(viqu_info->viqu_environ);
-		viqu_info->viqu_environ = NULL;
+			viqu_info->viqu_environ = NULL;
 
 		viqu_free_ptr((void **)viqu_info->viqu_cmd_buf);
 		if (viqu_info->viqu_read_fd > 2)
