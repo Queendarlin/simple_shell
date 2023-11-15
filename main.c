ff0ae3f5200ce02636fa475da202d572ec0dcbb7
Queendarlin <queendarlinnnamani@gmail.com>
2023-11-15 06:54:42 -0800

    Debuging

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Function to initialize and execute the shell program
- * @ac: The parameter for the count of command-line arguments
- * @av: Pointer to array of strings representing the command-line arguments
+ * @viqu_ac: The parameter for the count of command-line arguments
+ * @viqu_av: Pointer to array of strings
+ * representing the command-line arguments
  * Return: 0 upon successful execution, 1 if an error occurs
  */
 
-int main(int ac, char **av)
+int main(int viqu_ac, char **viqu_av)
 {
 	info_t viqu_info[] = { VIQU_DEFAULT_INIT };
 
@@ -17,29 +18,28 @@ int main(int ac, char **av)
 			"add $3, %0"
 			: "=r" (viqu_fd)
 			: "r" (viqu_fd));
-	if (ac == 2)
+	if (viqu_ac == 2)
 	{
-		viqu_fd = open(av[1], O_RDONLY);
+		viqu_fd = open(viqu_av[1], O_RDONLY);
 		if (viqu_fd == -1)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
+			switch (errno)
 			{
-				viqu_puts_error(av[0]);
-				viqu_puts_error(": 0: Can't open ");
-				viqu_puts_error(av[1]);
-				viqu_putchar_error('\n');
-				viqu_putchar_error(VIQU_FLUSH_BUFFER);
-				exit(127);
+				case EACCES:
+					exit(126);
+				case ENOENT:
+					viqu_puts_error(viqu_av[0]);
+					viqu_puts_error(": 0: Can't open ");
+					viqu_puts_error(viqu_av[1]);
+					exit(127);
+				default:
+					return (EXIT_FAILURE);
 			}
-			return (EXIT_FAILURE);
 		}
 		viqu_info->viqu_read_fd = viqu_fd;
 	}
-
 	viqu_occupy_environ_list(viqu_info);
 	viqu_read_file_hist(viqu_info);
-	viqu_shell_loop(viqu_info, av);
+	viqu_shell_loop(viqu_info, viqu_av);
 	return (EXIT_SUCCESS);
 }
